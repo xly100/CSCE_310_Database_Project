191cf278510ec24a5290dd3845f3af0b7407b606
X <102485629+xly100@users.noreply.github.com>
2022-12-04 15:12:45 -0600

    Linked edit button to u_profile_edit, added JS file for profile pages

---
+++ u_profile.js
@@ -0,0 +1,7 @@
+function getUserInfo(){ //Needs to obtain data from user table to display on u_profile and u_profile_edit
+	
+}
+
+function saveProfileDetails(){ //Sends new profile details to database, then goes from u_profile_edit to u_profile. Called by "save button"
+	
+}
