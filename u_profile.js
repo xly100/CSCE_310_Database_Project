e61e6ee97e9492d0fd4615c45ca1ebf7e04e2558
X <102485629+xly100@users.noreply.github.com>
2022-12-04 16:11:34 -0600

    Trying to write code that displays a table depending on usertype

--- u_profile.js
+++ u_profile.js
@@ -5,3 +5,19 @@ function getUserInfo(){ //Needs to obtain data from user table to display on u_p
 function saveProfileDetails(){ //Sends new profile details to database, then goes from u_profile_edit to u_profile. Called by "save button"
 	
 }
+
+function getUserType(){ //Obtains usertype of whoever is logged in
+	
+}
+
+function displayExtras(){ //Displays extra info depending on usertype
+	//Obtain usertype value here, set to var usertype
+	document.getElementByID("tableP").style.display = "block";
+	document.getElementByID("tableD").style.display = "block";
+	
+	if(usertype == "p"){
+		document.getElementByID("tableP").style.display = "none";
+	} else if(usertype == "d"){
+		document.getElementByID("tableD").style.display = "none";
+	}
+}
