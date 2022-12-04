6f18f214ae3042c56a7f8765cd6815034e066dec
X <102485629+xly100@users.noreply.github.com>
2022-12-04 16:27:54 -0600

    Extra info now changes depending on usertype

--- u_profile.js
+++ u_profile.js
@@ -12,12 +12,17 @@ function getUserType(){ //Obtains usertype of whoever is logged in
 
 function displayExtras(){ //Displays extra info depending on usertype
 	//Obtain usertype value here, set to var usertype
-	document.getElementByID("tableP").style.display = "block";
-	document.getElementByID("tableD").style.display = "block";
+	var usertype = "p";
+	
+	document.getElementById("tableP").style.display = "none";
+	document.getElementById("tableD").style.display = "none";
 	
 	if(usertype == "p"){
-		document.getElementByID("tableP").style.display = "none";
+		document.getElementById("tableP").style.display = "block";
 	} else if(usertype == "d"){
-		document.getElementByID("tableD").style.display = "none";
+		document.getElementById("tableD").style.display = "block";
 	}
 }
+window.addEventListener('DOMContentLoaded', (event) => {
+    displayExtras();
+});
