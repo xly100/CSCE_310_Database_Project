b4c8eecc9ea78738d3d72a5cc7a3e0bc14540380
Nooby9 <50778105+Nooby9@users.noreply.github.com>
2022-12-05 15:19:24 -0600

    Updated comment and user management javascript

---
+++ comment/commentAdmin.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    populateComments();
+});
+
+function populateComments(){
+
+    var id = '4';
+    var time = '11/09/2022 9:23:12';
+    var comment = 'No, U';
+    var name = 'Joe Mama';
+    var type = 'Doctor';
+    var appointment = '1';
+
+    var row = document.createElement('tr');
+
+    row.innerHTML = "<th scope='row'>" + id + "<\/th><td>" + time + "<\/td><td>" + comment + "<\/td><td>" 
+    + name + "<\/td><td>" + type + "<\/td><td>" + appointment + "<\/td>" + "<td><button type='button' class='btn btn-danger' onclick='deleteComments(this)'>Remove</button></td>";
+
+    const table = document.getElementById('commentTable');
+    table.appendChild(row);
+}
+
+function deleteComments(btn){
+    var row = btn.parentNode.parentNode;
+    removedId = row.id; // this is the id for the removed user
+    console.log(removedId);
+    row.parentNode.removeChild(row)
+
+    //remove comment from database
+}
--- userManagement/userManage.js
+++ userManagement/userManage.js
@@ -6,14 +6,17 @@ function populateUsers(){
 
     //todo: add php logic to database
     var id = '3';
-    var name = 'Jorge Bosh';
+    var firstName = 'Jorge';
+    var lastName = 'Bosh';
+    var name = firstName+' '+lastName;
+    var userName = 'jb1';
     var phone = '1236549870';
     var role = 'Doctor';
     
     var row = document.createElement('tr');
     row.setAttribute("id", id)
 
-    row.innerHTML = "<td>" + id + "<\/td><td>" + name + "<\/td><td>" + phone + "<\/td><td>" + role + "<\/td><td>" 
+    row.innerHTML = "<td>" + id + "<\/td><td>" + name + "<\/td><td>"+ userName +"<\/td><td>"+ phone + "<\/td><td>" + role + "<\/td><td>" 
     + "<a href='#' class='settings' title='Settings' data-toggle='tooltip' onclick='editUserProfile(this)'><i class='material-icons'>&#xE8B8;<\/i><\/a><a href='#' class='delete' title='Delete' data-toggle='tooltip' onclick='deleteUsers(this)'><i class='material-icons'>&#xE5C9;<\/i><\/a><\/td>";
 
     const table = document.getElementById('userTable');
