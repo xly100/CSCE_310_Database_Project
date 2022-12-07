08be7a74834f85e094dd7555bafa7e29a78be669
Nooby9 <50778105+Nooby9@users.noreply.github.com>
2022-12-07 01:08:26 -0600

    Shurui admin adduser now works with database

--- adduser.js
+++ adduser.js
@@ -13,16 +13,58 @@ function userInfo(){
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value; 
 
+    //connect to php code
+	var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        result = this.responseText;
+		if (result === "NO_RESULTS"){
+			alert("Invalid username or password");
+			return;
+		} else{
+            console.log(result);
+            addUserPatientDoctor(result,usertype);
+		}
+      }
+    };
+    xmlhttp.open("GET", "https://csce310database.000webhostapp.com/adduser.php?firstname=" + encodeURIComponent(firstName) + "&lastname=" + encodeURIComponent(lastName)
+    +"&phone="+encodeURIComponent(phone)+"&username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password)+"&usertype="+encodeURIComponent(usertype), true);
+    xmlhttp.send();
     
+    //end php
 
     console.log(firstName+lastName)
     console.log(phone)
     console.log(usertype)
-    console.log(email)
+    console.log(username)
     console.log(password)
 
-    window.location.href = ADMIN_PAGE;
+    setTimeout(function () {
+        window.location.href = ADMIN_PAGE; //will redirect to your blog page (an ex: blog.html)
+     }, 2000);
+    
+}
+
+function addUserPatientDoctor(userid,usertype){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        result = this.responseText;
+        //alert(result);
+      }
+    };
+
+    if(usertype=="p"){
+        xmlhttp.open("GET", "https://csce310database.000webhostapp.com/addpatient.php?userid=" + encodeURIComponent(userid), true);
+        xmlhttp.send();
+    }
+    
+    else if(usertype=="d"){
+        xmlhttp.open("GET", "https://csce310database.000webhostapp.com/adddoctor.php?userid=" + encodeURIComponent(userid), true);
+        xmlhttp.send();
+    }
+    
 }
